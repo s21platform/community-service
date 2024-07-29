de416787393431c9ee1e10aef4d117d7f75000bd
juliane <juliaane.grebe@gmail.com>
2024-07-29 23:27:34 +0300

    #7 add db connection

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/s21platform/community-service/internal/config"
+import (
+	"fmt"
+	"github.com/s21platform/community-service/internal/config"
+	"github.com/s21platform/community-service/internal/repository/postgres"
+)
 
 func main() {
-	_ = config.MustLoad()
+
+	cfg := config.MustLoad()
+	db := postgres.New(cfg)
+	_ = db
+	fmt.Println(cfg)
 }
