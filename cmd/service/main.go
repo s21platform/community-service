9863c492c61ae810a6ef4d8342a374d895bbfad9
asepopov <asepopov@avito.ru>
2024-10-12 18:09:15 +0300

    refactoring service -> rpc

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -10,14 +10,14 @@ import (
 	communityproto "github.com/s21platform/community-proto/community-proto"
 	"github.com/s21platform/community-service/internal/config"
 	"github.com/s21platform/community-service/internal/repository/postgres"
-	"github.com/s21platform/community-service/internal/service"
+	"github.com/s21platform/community-service/internal/rpc"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	dbRepo := postgres.New(cfg)
 
-	thisService := service.New(dbRepo)
+	thisService := rpc.New(dbRepo)
 
 	s := grpc.NewServer()
 	communityproto.RegisterCommunityServiceServer(s, thisService)
