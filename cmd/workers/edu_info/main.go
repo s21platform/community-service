c6dfd49760a402381ce9daaa25a2b65cfabbe089
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-11-27 21:29:10 +0300

    fix: campus worker fixed

--- cmd/workers/edu_info/main.go
+++ cmd/workers/edu_info/main.go
@@ -2,32 +2,28 @@ package main
 
 import (
 	"context"
-	"log"
-	"time"
-)
+	"sync"
 
-func worker(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("Worker is stopping...")
-			return 
-		case <-time.After(30 * 24 * time.Hour): 
-			log.Println("work")
-		}
-	}
-}
+	"github.com/s21platform/community-service/internal/service/campus"
+)
 
 func main() {
 	// Создаём контекст с отменой
 	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+
+	campusWorker := campus.New()
+
+	wg.Add(1)
 
 	// Запускаем горутину
-	go worker(ctx)
+	go func() {
+		campusWorker.Run(ctx, &wg)
+	}()
 
 	// Завершаем работу воркера через контекст
 	cancel()
 
 	// Даем время воркеру завершиться
-	time.Sleep(1 * time.Second)
+	wg.Wait()
 }
