f4944141491231983dff3652cc3ff7cecf58acba
zolotova.polina3 <zolotova.polina3@wb.ru>
2024-12-08 23:07:06 +0300

    some tests

--- go.mod
+++ go.mod
@@ -10,16 +10,19 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/s21platform/community-proto v0.0.2
 	github.com/s21platform/metrics-lib v0.0.6
+	github.com/stretchr/testify v1.2.2
 	golang.org/x/net v0.26.0
 	google.golang.org/grpc v1.65.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/marpaia/graphite-golang v0.0.0-20190519024811-caf161d2c2b1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
--- internal/rpc/service_test.go
+++ internal/rpc/service_test.go
@@ -1,21 +1,95 @@
 package rpc_test
 
 import (
+	"errors"
 	"github.com/golang/mock/gomock"
-	"github.com/s21platform/community-service/internal/config"
+	community_proto "github.com/s21platform/community-proto/community-proto"
+	"github.com/s21platform/community-service/internal/model"
 	"github.com/s21platform/community-service/internal/rpc"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"testing"
 )
 
 func TestServer_GetPeerSchoolData(t *testing.T) {
 	t.Parallel()
-
 	ctx := context.Background()
-	uuid := "test_uuid"
-	ctx = context.WithValue(ctx, config.KeyUUID, uuid)
 
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 	mockRepo := rpc.NewMockDbRepo(controller)
+
+	t.Run("get_peer_school_data_ok", func(t *testing.T) {
+		expectedData := model.PeerSchoolData{ClassName: "test-class", ParallelName: "test-parallel"}
+		nickName := "aboba"
+		mockRepo.EXPECT().GetPeerSchoolData(gomock.Any(), nickName).Return(expectedData, nil)
+
+		s := rpc.New(mockRepo)
+		data, err := s.GetPeerSchoolData(ctx, &community_proto.GetSchoolDataIn{NickName: nickName})
+		assert.NoError(t, err)
+		assert.Equal(t, data, &community_proto.GetSchoolDataOut{ClassName: expectedData.ClassName, ParallelName: expectedData.ParallelName})
+	})
+
+	t.Run("get_peer_school_data_err", func(t *testing.T) {
+		nickName := "aboba"
+		expectedErr := errors.New("select err")
+		mockRepo.EXPECT().GetPeerSchoolData(gomock.Any(), nickName).Return(model.PeerSchoolData{}, expectedErr)
+
+		s := rpc.New(mockRepo)
+
+		data, err := s.GetPeerSchoolData(ctx, &community_proto.GetSchoolDataIn{NickName: nickName})
+		assert.Nil(t, data)
+		st, ok := status.FromError(err)
+		assert.True(t, ok)
+		assert.Equal(t, codes.Internal, st.Code())
+		assert.Contains(t, st.Message(), "select err")
+	})
+}
+
+func TestServer_IsPeerExist(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	mockRepo := rpc.NewMockDbRepo(controller)
+
+	t.Run("get_peer_status_ok", func(t *testing.T) {
+		expectedStatus := "ACTIVE"
+		email := "aboba@student.21-school.ru"
+		mockRepo.EXPECT().GetPeerStatus(gomock.Any(), email).Return(expectedStatus, nil)
+
+		s := rpc.New(mockRepo)
+		isExist, err := s.IsPeerExist(ctx, &community_proto.EmailIn{Email: email})
+		assert.NoError(t, err)
+		assert.True(t, isExist.IsExist)
+	})
+
+	t.Run("get_peer_status_not_found", func(t *testing.T) {
+		expectedStatus := "NOT ACTIVE"
+		email := "aboba@student.21-school.ru"
+		mockRepo.EXPECT().GetPeerStatus(gomock.Any(), email).Return(expectedStatus, nil)
+
+		s := rpc.New(mockRepo)
+		isExist, err := s.IsPeerExist(ctx, &community_proto.EmailIn{Email: email})
+		assert.NoError(t, err)
+		assert.False(t, isExist.IsExist)
+	})
+
+	t.Run("get_peer_status_err", func(t *testing.T) {
+		email := "aboba@student.21-school.ru"
+		expectedErr := errors.New("select err")
+		mockRepo.EXPECT().GetPeerStatus(gomock.Any(), email).Return("", expectedErr)
+
+		s := rpc.New(mockRepo)
+		isExist, err := s.IsPeerExist(ctx, &community_proto.EmailIn{Email: email})
+
+		assert.Nil(t, isExist)
+		st, ok := status.FromError(err)
+		assert.True(t, ok)
+		assert.Equal(t, codes.Internal, st.Code())
+		assert.Contains(t, st.Message(), "select err")
+	})
 }
