bfb94c73149a90dede3cf5aa91ff7b25b6339d20
zolotova.polina3 <zolotova.polina3@wb.ru>
2024-12-31 17:06:57 +0300

    fix migration query

--- migrations/0004_create_login_table.sql
+++ migrations/0004_create_login_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS login
     id SERIAL PRIMARY KEY,
     nickname VARCHAR NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT NOW()
-)
+);
 
 -- +goose Down
-DROP TABLE IF EXISTS logins
+DROP TABLE IF EXISTS login
